617dec97c0c4678954321b766f026f2011f732c9
patrick551 <44680453+patrick551@users.noreply.github.com>
2018-11-02 11:55:30 -0500

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnValidate = New System.Windows.Forms.Button()
+        Me.txtDate = New System.Windows.Forms.TextBox()
+        Me.label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnValidate
+        '
+        Me.btnValidate.Location = New System.Drawing.Point(20, 85)
+        Me.btnValidate.Name = "btnValidate"
+        Me.btnValidate.Size = New System.Drawing.Size(75, 23)
+        Me.btnValidate.TabIndex = 8
+        Me.btnValidate.Text = "Validate"
+        Me.btnValidate.UseVisualStyleBackColor = True
+        '
+        'txtDate
+        '
+        Me.txtDate.Location = New System.Drawing.Point(20, 48)
+        Me.txtDate.Name = "txtDate"
+        Me.txtDate.Size = New System.Drawing.Size(230, 20)
+        Me.txtDate.TabIndex = 7
+        '
+        'label1
+        '
+        Me.label1.AutoSize = True
+        Me.label1.Location = New System.Drawing.Point(17, 22)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(71, 13)
+        Me.label1.TabIndex = 6
+        Me.label1.Text = "Date Of Birth:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(267, 128)
+        Me.Controls.Add(Me.btnValidate)
+        Me.Controls.Add(Me.txtDate)
+        Me.Controls.Add(Me.label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Private WithEvents btnValidate As System.Windows.Forms.Button
+    Private WithEvents txtDate As System.Windows.Forms.TextBox
+    Private WithEvents label1 As System.Windows.Forms.Label
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Globalization
+Imports System.Text.RegularExpressions
+
+Public Class Form1
+    Private Sub btnValidate_Click(sender As System.Object, e As System.EventArgs) Handles btnValidate.Click
+        Dim regex As Regex = New Regex("(((0|1)[0-9]|2[0-9]|3[0-1])\/(0[1-9]|1[0-2])\/((19|20)\d\d))$")
+
+        'Verify whether date entered in dd/MM/yyyy format.
+        Dim isValid As Boolean = regex.IsMatch(txtDate.Text.Trim)
+
+        'Verify whether entered date is Valid date.
+        Dim dt As DateTime
+        isValid = DateTime.TryParseExact(txtDate.Text, "dd/MM/yyyy", New CultureInfo("en-GB"), DateTimeStyles.None, dt)
+        If Not isValid Then
+            MessageBox.Show("Invalid Date.")
+        End If
+    End Sub
+End Class
